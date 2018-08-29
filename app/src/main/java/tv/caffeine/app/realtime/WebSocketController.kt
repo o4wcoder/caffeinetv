864757fe44904ce18e58c6c719c09a0d4eec1af7
bolot <bolot@caffeine.tv>
2018-08-29 15:47:10 -0700

    Send keep-alive every 30 seconds

--- app/src/main/java/tv/caffeine/app/realtime/WebSocketController.kt
+++ app/src/main/java/tv/caffeine/app/realtime/WebSocketController.kt
@@ -3,10 +3,17 @@ package tv.caffeine.app.realtime
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.timeunit.TimeUnit
 import okhttp3.*
 import timber.log.Timber
 
 private const val STATUS_CODE_NORMAL_CLOSURE = 1000
+
+private const val HEALZ = "\"HEALZ\""
+private const val THANKS = "\"THANKS\""
 
 class WebSocketController {
 
@@ -24,17 +31,25 @@ class WebSocketController {
     }
 
     private class Listener(private val headers: String, val callback: (String) -> Unit): WebSocketListener() {
-        val gsonForHandshake: Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
-        val gsonForEvents: Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
-        var messageNumber: Int = 0
+        private val gsonForHandshake: Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+        private var messageNumber: Int = 0
+        private var keepAlive: Job? = null
 
         override fun onOpen(webSocket: WebSocket?, response: Response?) {
             Timber.d("Opened, response = $response")
             webSocket?.send(headers)
+            keepAlive = launch {
+                while(true) {
+                    delay(30, TimeUnit.SECONDS)
+                    Timber.d("About to send a heartbeat")
+                    webSocket?.send(HEALZ)
+                }
+            }
         }
 
         override fun onMessage(webSocket: WebSocket?, text: String?) {
             Timber.d("Got message $text")
+            if (text == THANKS) return
             when (messageNumber++) {
                 0 -> text?.let { gsonForHandshake.fromJson(it, HandshakeMessage::class.java) }
                 1 -> text?.let { callback(it) }
@@ -43,6 +58,7 @@ class WebSocketController {
 
         override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
             Timber.d("Closing, code = $code, reason = $reason")
+            keepAlive?.cancel()
         }
 
     }
