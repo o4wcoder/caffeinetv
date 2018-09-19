e50f26fcca29037a01768fe1b403518a0bb97689
bolot <bolot@caffeine.tv>
2018-09-19 15:15:13 -0700

    Refresh lobby every 30 seconds

--- app/src/main/java/tv/caffeine/app/lobby/LobbyFragment.kt
+++ app/src/main/java/tv/caffeine/app/lobby/LobbyFragment.kt
@@ -10,10 +10,9 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.fragment_lobby.*
 import tv.caffeine.app.R
-import tv.caffeine.app.api.AccountsService
 import tv.caffeine.app.auth.TokenStore
+import tv.caffeine.app.databinding.FragmentLobbyBinding
 import tv.caffeine.app.di.ViewModelFactory
 import tv.caffeine.app.session.FollowManager
 import javax.inject.Inject
@@ -24,46 +23,45 @@ class LobbyFragment : DaggerFragment() {
     @Inject lateinit var followManager: FollowManager
     private val viewModelProvider by lazy { ViewModelProviders.of(this, viewModelFactory) }
     @Inject lateinit var lobbyAdapter: LobbyAdapter
-    @Inject lateinit var accountsService: AccountsService
     @Inject lateinit var tokenStore: TokenStore
+    private var viewModel: LobbyViewModel? = null
+    private var binding: FragmentLobbyBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (tokenStore.createRefreshTokenBody() == null) {
             findNavController().navigate(R.id.action_lobbyFragment_to_landingFragment)
         }
+        viewModel = viewModelProvider.get(LobbyViewModel::class.java).apply {
+            lobby.observe(this@LobbyFragment, Observer {
+                val items = LobbyItem.parse(it)
+                lobbyAdapter.submitList(items, it.tags, it.content)
+                binding?.lobbySwipeRefreshLayout?.isRefreshing = false
+            })
+        }
+        refreshLobby()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lobby, container, false)
+        val binding = FragmentLobbyBinding.inflate(inflater, container, false).apply {
+            lobbyRecyclerView.run {
+                adapter = lobbyAdapter
+                setRecycledViewPool(lobbyAdapter.recycledViewPool)
+            }
+            profileButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.myProfileFragment))
+            searchButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.exploreFragment))
+            activityButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.notificationsFragment))
+            lobbySwipeRefreshLayout.setOnRefreshListener { refreshLobby() }
+        }
+        this.binding = binding
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        lobby_recycler_view.adapter = lobbyAdapter
-        lobby_recycler_view.setRecycledViewPool(lobbyAdapter.recycledViewPool)
-        profileButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.myProfileFragment))
-        searchButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.exploreFragment))
-        activityButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.notificationsFragment))
-        loadLobby()
-    }
-
-    private fun loadLobby() {
-        val viewModel = viewModelProvider.get(LobbyViewModel::class.java)
-        viewModel.lobby.observe(this, Observer {
-            val items = LobbyItem.parse(it)
-            lobbyAdapter.submitList(items, it.tags, it.content)
-            lobby_swipe_refresh_layout.isRefreshing = false
-        })
-        lobby_swipe_refresh_layout.setOnRefreshListener { refreshLobby(viewModel) }
-        refreshLobby(viewModel)
-    }
-
-    private fun refreshLobby(viewModel: LobbyViewModel) {
+    private fun refreshLobby() {
         followManager.refreshFollowedUsers()
-        viewModel.refresh()
+        viewModel?.refresh()
     }
 
 }
--- app/src/main/java/tv/caffeine/app/lobby/LobbyViewModel.kt
+++ app/src/main/java/tv/caffeine/app/lobby/LobbyViewModel.kt
@@ -2,18 +2,29 @@ package tv.caffeine.app.lobby
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
 import tv.caffeine.app.api.Api
 import tv.caffeine.app.api.LobbyService
+import java.util.concurrent.TimeUnit
 
 class LobbyViewModel(private val lobbyService: LobbyService) : ViewModel() {
     val lobby: MutableLiveData<Api.v3.Lobby.Result> = MutableLiveData()
+    private var job: Job? = null
 
     fun refresh() {
-        loadLobby()
+        job?.cancel()
+        job = launch {
+            while(isActive) {
+                loadLobby()
+                delay(30, TimeUnit.SECONDS)
+            }
+        }
     }
 
     private fun loadLobby() {
@@ -27,5 +38,10 @@ class LobbyViewModel(private val lobbyService: LobbyService) : ViewModel() {
                 response?.body()?.let { lobby.value = it }
             }
         })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
     }
 }
