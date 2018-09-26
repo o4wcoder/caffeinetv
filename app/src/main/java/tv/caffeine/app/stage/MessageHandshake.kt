0efedca2b4fe558207a4a405859b48da68473075
bolot <bolot@caffeine.tv>
2018-09-26 16:41:04 -0700

    Better websocket management and cleanup

--- app/src/main/java/tv/caffeine/app/stage/MessageHandshake.kt
+++ app/src/main/java/tv/caffeine/app/stage/MessageHandshake.kt
@@ -12,13 +12,15 @@ import javax.inject.Inject
 class MessageHandshake @Inject constructor(
         private val tokenStore: TokenStore
 ) {
-    private val webSocketController = WebSocketController("msg")
+    private var webSocketController: WebSocketController? = null
     private val gson: Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
 
     fun connect(stageIdentifier: String, callback: (Api.Message) -> Unit) {
         val url = "wss://realtime.caffeine.tv/v2/reaper/stages/$stageIdentifier/messages"
         val headers = tokenStore.webSocketHeader()
-        webSocketController.open(url, headers) {
+        webSocketController?.close()
+        webSocketController = WebSocketController("msg")
+        webSocketController?.open(url, headers) {
             Timber.d("Received message $it")
             val message = gson.fromJson(it, Api.Message::class.java)
             callback(message)
@@ -27,7 +29,8 @@ class MessageHandshake @Inject constructor(
 
     fun close() {
         Timber.d("msg - closing handshake handler")
-        webSocketController.close()
+        webSocketController?.close()
+        webSocketController = null
     }
 
 }
