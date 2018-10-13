c5ed17fb6ce7e1d973b3ecc7b590a90f3387b9c1
bolot <bolot@caffeine.tv>
2018-10-13 11:36:24 -0700

    Preferences navigation

--- app/src/main/java/tv/caffeine/app/settings/SettingsFragment.kt
+++ app/src/main/java/tv/caffeine/app/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package tv.caffeine.app.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceScreen
@@ -8,17 +9,25 @@ import timber.log.Timber
 import tv.caffeine.app.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
-        findPreference("tos").setOnPreferenceClickListener {
+        findPreference(rootKey)?.title?.let { title ->
+            (activity as? AppCompatActivity)?.supportActionBar?.title = title
+        }
+        configureLegalDocs()
+    }
+
+    private fun configureLegalDocs() {
+        findPreference("tos")?.setOnPreferenceClickListener {
             openLegalDoc(LegalDoc.TOS)
             return@setOnPreferenceClickListener true
         }
-        findPreference("privacy").setOnPreferenceClickListener {
+        findPreference("privacy")?.setOnPreferenceClickListener {
             openLegalDoc(LegalDoc.PrivacyPolicy)
             return@setOnPreferenceClickListener true
         }
-        findPreference("guidelines").setOnPreferenceClickListener {
+        findPreference("guidelines")?.setOnPreferenceClickListener {
             openLegalDoc(LegalDoc.CommunityGuidelines)
             return@setOnPreferenceClickListener true
         }
@@ -28,4 +37,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val action = SettingsFragmentDirections.actionSettingsFragmentToLegalDocsFragment(legalDoc.ordinal)
         findNavController().navigate(action)
     }
+
+    override fun onNavigateToScreen(preferenceScreen: PreferenceScreen?) {
+        super.onNavigateToScreen(preferenceScreen)
+        preferenceScreen?.key?.let { key ->
+            val args = Bundle().apply { putString(ARG_PREFERENCE_ROOT, key) }
+            findNavController().navigate(R.id.action_settingsFragment_self, args)
+        }
+        Timber.d("Navigating to ${preferenceScreen?.title}")
+    }
+
 }
