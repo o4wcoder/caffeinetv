b957b89f4ac61490ea64e07d5c567210da7d1b37
bolot <bolot@caffeine.tv>
2018-10-16 16:42:09 -0700

    Add UsersService.disconnectIdentity

--- app/src/main/java/tv/caffeine/app/api/UsersService.kt
+++ app/src/main/java/tv/caffeine/app/api/UsersService.kt
@@ -2,11 +2,13 @@ package tv.caffeine.app.api
 
 import kotlinx.coroutines.Deferred
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import tv.caffeine.app.api.model.CaidRecord
+import tv.caffeine.app.api.model.IdentityProvider
 import tv.caffeine.app.api.model.SignedUserToken
 import tv.caffeine.app.api.model.UserContainer
 
@@ -34,6 +36,7 @@ interface UsersService {
 
     @GET("v1/users/{caid}/ignores")
     fun listIgnoredUsers(@Path("caid") userId: String): Deferred<List<CaidRecord.IgnoreRecord>>
+
+    @DELETE("v1/users/{caid}/connected_accounts/{connectedAccount}")
+    fun disconnectIdentity(@Path("caid") userId: String, @Path("connectedAccount") identityProvider: IdentityProvider): Deferred<Response<Any>>
 }
-
-
