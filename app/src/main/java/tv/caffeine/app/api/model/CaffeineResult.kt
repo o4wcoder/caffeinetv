4fd43392da0af7e19326984334e5864c83c58879
bolot <bolot@caffeine.tv>
2018-10-25 08:37:37 -0700

    CaffeineResult and Deferred<Response<T>> integration

--- app/src/main/java/tv/caffeine/app/api/model/CaffeineResult.kt
+++ app/src/main/java/tv/caffeine/app/api/model/CaffeineResult.kt
@@ -1,9 +1,23 @@
 package tv.caffeine.app.api.model
 
+import com.google.gson.Gson
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import tv.caffeine.app.api.ApiErrorResult
 
 sealed class CaffeineResult<T> {
     class Success<T>(val value: T) : CaffeineResult<T>()
     class Error<T>(val error: ApiErrorResult) : CaffeineResult<T>()
     class Failure<T>(val exception: Throwable) : CaffeineResult<T>()
+}
+
+suspend fun <T> Deferred<Response<T>>.awaitAndParseErrors(gson: Gson): CaffeineResult<T> {
+    val response = await()
+    val body = response.body()
+    val errorBody = response.errorBody()
+    return when {
+        response.isSuccessful && body != null -> CaffeineResult.Success(body)
+        errorBody != null -> CaffeineResult.Error(gson.fromJson(errorBody.string(), ApiErrorResult::class.java))
+        else -> CaffeineResult.Failure(Exception("awaitAndParseErrors"))
+    }
 }
--- app/src/main/java/tv/caffeine/app/lobby/LoadLobbyUseCase.kt
+++ app/src/main/java/tv/caffeine/app/lobby/LoadLobbyUseCase.kt
@@ -1,24 +1,16 @@
 package tv.caffeine.app.lobby
 
 import com.google.gson.Gson
-import tv.caffeine.app.api.ApiErrorResult
 import tv.caffeine.app.api.LobbyService
 import tv.caffeine.app.api.model.CaffeineResult
 import tv.caffeine.app.api.model.Lobby
+import tv.caffeine.app.api.model.awaitAndParseErrors
 import javax.inject.Inject
 
 class LoadLobbyUseCase @Inject constructor(
         private val lobbyService: LobbyService,
         private val gson: Gson
 ) {
-    suspend operator fun invoke(): CaffeineResult<Lobby> {
-        val response = lobbyService.loadLobby().await()
-        val lobby = response.body()
-        val errorBody = response.errorBody()
-        return when {
-            response.isSuccessful && lobby != null -> CaffeineResult.Success(lobby)
-            errorBody != null -> CaffeineResult.Error(gson.fromJson(errorBody.string(), ApiErrorResult::class.java))
-            else -> CaffeineResult.Failure(Exception("Unknown state"))
-        }
-    }
+    suspend operator fun invoke(): CaffeineResult<Lobby> =
+            lobbyService.loadLobby().awaitAndParseErrors(gson)
 }
