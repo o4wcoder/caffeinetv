f6ab078744c0f0f261e7a41c1fca67934f24a1ec
bolot <bolot@caffeine.tv>
2018-10-28 17:49:27 -0700

    Connect the buy gold with credits button

--- app/src/main/java/tv/caffeine/app/settings/GoldAndCreditsFragment.kt
+++ app/src/main/java/tv/caffeine/app/settings/GoldAndCreditsFragment.kt
@@ -1,13 +1,20 @@
 package tv.caffeine.app.settings
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.parcel.Parcelize
 import tv.caffeine.app.databinding.FragmentGoldAndCreditsBinding
 import tv.caffeine.app.profile.WalletViewModel
 import tv.caffeine.app.ui.CaffeineFragment
+
+@Parcelize
+enum class BuyGoldOption: Parcelable {
+    UsingPlayStore, UsingCredits
+}
 
 class GoldAndCreditsFragment : CaffeineFragment() {
     private lateinit var binding: FragmentGoldAndCreditsBinding
@@ -27,9 +34,12 @@ class GoldAndCreditsFragment : CaffeineFragment() {
             val action = GoldAndCreditsFragmentDirections.actionGoldAndCreditsFragmentToTransactionHistoryFragment()
             findNavController().navigate(action)
         }
-        binding.buyGoldButton.setOnClickListener {
-            val action = GoldAndCreditsFragmentDirections.actionGoldAndCreditsFragmentToGoldBundlesFragment()
-            findNavController().navigate(action)
-        }
+        binding.buyGoldButton.setOnClickListener { navigateToBuyGold(BuyGoldOption.UsingPlayStore) }
+        binding.buyGoldWithCreditsButton.setOnClickListener { navigateToBuyGold(BuyGoldOption.UsingCredits) }
+    }
+
+    private fun navigateToBuyGold(buyGoldOption: BuyGoldOption) {
+        val action = GoldAndCreditsFragmentDirections.actionGoldAndCreditsFragmentToGoldBundlesFragment(buyGoldOption)
+        findNavController().navigate(action)
     }
 }
