b41f2fbd14de827de62dc3c5bec6bab3f81d7ff7
bolot <bolot@users.noreply.github.com>
2018-12-06 11:12:55 -0800

    Update libraries (#43)
    * Update Android Studio, Kotlin, and other libraries

--- app/src/main/java/tv/caffeine/app/di/CaffeineComponent.kt
+++ app/src/main/java/tv/caffeine/app/di/CaffeineComponent.kt
@@ -3,12 +3,14 @@ package tv.caffeine.app.di
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import tv.caffeine.app.CaffeineApplication
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [
     AndroidInjectionModule::class,
+    AndroidSupportInjectionModule::class,
     InjectionModule::class,
     AppModule::class,
     ViewModelModule::class,
