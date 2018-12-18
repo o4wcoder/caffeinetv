b684b3c69537597962515748ef3f799cf4590626
bolot <bolot@users.noreply.github.com>
2018-12-18 00:07:00 -0800

    Gold bundles v2 (#141)

--- app/src/debug/java/tv/caffeine/app/di/BillingClientFactory.kt
+++ app/src/debug/java/tv/caffeine/app/di/BillingClientFactory.kt
@@ -18,27 +18,27 @@ object BillingClientFactory {
         BillingStore.defaultStore(context)
                 .clearProducts()
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy10", BillingClient.SkuType.INAPP, "0.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy10", BillingClient.SkuType.INAPP, "0.99",
                                 990000L, "USD", "10 Gold Bundle", "10 Gold Bundle").build()
                 )
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy50", BillingClient.SkuType.INAPP, "4.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy50", BillingClient.SkuType.INAPP, "4.99",
                                 4990000L, "USD", "50 Gold Bundle", "50 Gold Bundle").build()
                 )
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy100", BillingClient.SkuType.INAPP, "9.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy100", BillingClient.SkuType.INAPP, "9.99",
                                 9990000L, "USD", "100 Gold Bundle", "100 Gold Bundle").build()
                 )
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy255", BillingClient.SkuType.INAPP, "24.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy255", BillingClient.SkuType.INAPP, "24.99",
                                 24990000L, "USD", "255 Gold Bundle", "255 Gold Bundle").build()
                 )
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy505", BillingClient.SkuType.INAPP, "49.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy505", BillingClient.SkuType.INAPP, "49.99",
                                 49990000L, "USD", "505 Gold Bundle", "505 Gold Bundle").build()
                 )
                 .addProduct(
-                        SkuDetailsBuilder("gold.bundle.v1.buy1010", BillingClient.SkuType.INAPP, "99.99",
+                        SkuDetailsBuilder("gold.bundle.v2.buy1010", BillingClient.SkuType.INAPP, "99.99",
                                 99990000L, "USD", "1010 Gold Bundle", "1010 Gold Bundle").build()
                 )
     }
