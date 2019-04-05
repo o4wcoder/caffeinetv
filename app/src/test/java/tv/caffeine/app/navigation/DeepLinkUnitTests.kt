9d052ec64df772baa4529a051a07f8a1ff012806
bolot <bolot@users.noreply.github.com>
2019-04-05 14:03:06 -0700

    Use Parameterized test runner to report each deep link separately (#433)
    * Use Parameterized test runner to report each deep link separately

--- app/src/test/java/tv/caffeine/app/navigation/DeepLinkUnitTests.kt
+++ app/src/test/java/tv/caffeine/app/navigation/DeepLinkUnitTests.kt
@@ -4,18 +4,18 @@ import android.content.Intent
 import android.content.res.Resources
 import android.net.Uri
 import android.os.Build.VERSION_CODES.O_MR1
+import androidx.annotation.IdRes
 import androidx.navigation.NavController
 import androidx.navigation.findDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.get
-import androidx.test.annotation.UiThreadTest
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
+import org.robolectric.ParameterizedRobolectricTestRunner
 import org.robolectric.annotation.Config
 import tv.caffeine.app.MainActivity
 import tv.caffeine.app.R
@@ -27,27 +27,32 @@ import tv.caffeine.app.di.InjectionActivityTestRule
  * DeepLinkUnitTests.setup() will fail with SDK 28 and Robolectric 4.2. SDK 27 works.
  * TODO: AND-140 to try the latest Robolectric library.
  */
-@RunWith(RobolectricTestRunner::class)
+@RunWith(ParameterizedRobolectricTestRunner::class)
 @Config(sdk = [O_MR1])
-class DeepLinkUnitTests {
+class DeepLinkUnitTests(private val url: String, @IdRes private val destination: Int) {
     private lateinit var navController: NavController
     private lateinit var resources: Resources
     private val activityTestRule = InjectionActivityTestRule(MainActivity::class.java, DaggerTestComponent.builder())
 
-    private val expectedDeepLinkDestinations: List<Pair<String, Int>> = listOf(
-            "https://www.caffeine.tv/account/claim-gold/index.html?id=token" to R.id.caffeineLinksFragment,
-            "https://www.caffeine.tv/account/reset-password/?code=code" to R.id.caffeineLinksFragment,
-            "https://www.caffeine.tv/account/reset-password?code=code" to R.id.caffeineLinksFragment,
-            "https://www.caffeine.tv/account/email-confirmation/index.html" to R.id.confirmEmailFragment,
-            "https://www.caffeine.tv/account/email-confirmation/index.html?code=code&caid=caid" to R.id.confirmEmailFragment,
-            "https://www.caffeine.tv/ESL" to R.id.stageFragment,
-            "https://www.caffeine.tv/ESL/profile" to R.id.profileFragment,
-            "https://www.caffeine.tv/caffeine" to R.id.stageFragment,
-            "https://www.caffeine.tv/caffeine?bst=sharing" to R.id.stageFragment,
-            "https://www.caffeine.tv/caffeine/profile" to R.id.profileFragment,
-            "https://www.caffeine.tv/privacy.html" to R.id.caffeineLinksFragment,
-            "https://www.caffeine.tv/tos.html" to R.id.caffeineLinksFragment
-    )
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun data() = listOf(
+                arrayOf("https://www.caffeine.tv/account/claim-gold/index.html?id=token", R.id.caffeineLinksFragment),
+                arrayOf("https://www.caffeine.tv/account/reset-password/?code=code", R.id.caffeineLinksFragment),
+                arrayOf("https://www.caffeine.tv/account/reset-password?code=code", R.id.caffeineLinksFragment),
+                arrayOf("https://www.caffeine.tv/account/email-confirmation/index.html", R.id.confirmEmailFragment),
+                arrayOf("https://www.caffeine.tv/account/email-confirmation/index.html?code=code&caid=caid", R.id.confirmEmailFragment),
+                arrayOf("https://www.caffeine.tv/ESL", R.id.stageFragment),
+                arrayOf("https://www.caffeine.tv/ESL/profile", R.id.profileFragment),
+                arrayOf("https://www.caffeine.tv/caffeine", R.id.stageFragment),
+                arrayOf("https://www.caffeine.tv/caffeine?bst=sharing", R.id.stageFragment),
+                arrayOf("https://www.caffeine.tv/caffeine/profile", R.id.profileFragment),
+                arrayOf("https://www.caffeine.tv/privacy.html", R.id.caffeineLinksFragment),
+                arrayOf("https://www.caffeine.tv/tos.html", R.id.caffeineLinksFragment)
+        )
+
+    }
 
     @Before
     fun setup() {
@@ -64,21 +69,18 @@ class DeepLinkUnitTests {
     }
 
     @Test
-    @UiThreadTest
-    fun `check all links`() {
+    fun `verify deep link`() {
         val navGraph = navController.graph
-        for ((url, destination) in expectedDeepLinkDestinations) {
-            val uri = Uri.parse(url)
-            val expectedDestination = navController.graph[destination]
-            val actualDestination = navGraph.findDestination(uri)
-            assertNotNull("unexpected null", actualDestination)
-            requireNotNull(actualDestination)
-            val expectedLabel = expectedDestination.label
-            val actualLabel = actualDestination.label
-            val expectedResource = resources.getResourceEntryName(expectedDestination.id)
-            val actualResource = resources.getResourceEntryName(actualDestination.id)
-            assertEquals("Deep link: $url; Destinations: Expected: $expectedLabel, actual: $actualLabel", expectedResource, actualResource)
-        }
+        val uri = Uri.parse(url)
+        val expectedDestination = navController.graph[destination]
+        val actualDestination = navGraph.findDestination(uri)
+        assertNotNull("unexpected null", actualDestination)
+        requireNotNull(actualDestination)
+        val expectedLabel = expectedDestination.label
+        val actualLabel = actualDestination.label
+        val expectedResource = resources.getResourceEntryName(expectedDestination.id)
+        val actualResource = resources.getResourceEntryName(actualDestination.id)
+        assertEquals("Deep link: $url; Destinations: Expected: $expectedLabel, actual: $actualLabel", expectedResource, actualResource)
     }
 
 }
