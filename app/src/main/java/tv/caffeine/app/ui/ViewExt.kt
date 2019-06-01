c08163fdef4c348951e31ef71d3476d6462e03eb
David Shi <DavidSnooker@gmail.com>
2019-05-31 17:40:31 -0700

    [FEAT-270] Update lobby card paddings and alignment (#557)
    * [FEAT-270] Update lobby card paddings and alignment

--- app/src/main/java/tv/caffeine/app/ui/ViewExt.kt
+++ app/src/main/java/tv/caffeine/app/ui/ViewExt.kt
@@ -1,7 +1,10 @@
 package tv.caffeine.app.ui
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
+
+fun View.isLayoutLtr() = layoutDirection == View.LAYOUT_DIRECTION_LTR
 
 @BindingAdapter("android:layout_width")
 fun View.setLayoutWidth(width: Float) {
@@ -11,4 +14,34 @@ fun View.setLayoutWidth(width: Float) {
 @BindingAdapter("android:layout_height")
 fun View.setLayoutHeight(height: Float) {
     layoutParams.height = height.toInt()
+}
+
+@BindingAdapter("android:layout_marginStart")
+fun View.setLayoutMarginStart(margin: Float) {
+    (layoutParams as ViewGroup.MarginLayoutParams).apply {
+        margin.toInt().let {
+            marginStart = it
+            if (isLayoutLtr()) leftMargin = it else rightMargin = it
+        }
+    }
+}
+
+@BindingAdapter("android:layout_marginEnd")
+fun View.setLayoutMarginEnd(margin: Float) {
+    (layoutParams as ViewGroup.MarginLayoutParams).apply {
+        margin.toInt().let {
+            marginEnd = it
+            if (isLayoutLtr()) rightMargin = it else leftMargin = it
+        }
+    }
+}
+
+@BindingAdapter("android:layout_marginTop")
+fun View.setLayoutMarginTop(margin: Float) {
+    (layoutParams as ViewGroup.MarginLayoutParams).topMargin = margin.toInt()
+}
+
+@BindingAdapter("android:layout_marginBottom")
+fun View.setLayoutMarginBottom(margin: Float) {
+    (layoutParams as ViewGroup.MarginLayoutParams).bottomMargin = margin.toInt()
 }
