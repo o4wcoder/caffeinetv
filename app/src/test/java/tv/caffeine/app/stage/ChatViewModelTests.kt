66414d5b455b67b5fcbb6ca2708b027a0967e390
bolot <bolot@users.noreply.github.com>
2019-07-02 17:35:34 -0700

    Add 2 more tests for ChatViewModel (#633)

--- app/src/test/java/tv/caffeine/app/stage/ChatViewModelTests.kt
+++ app/src/test/java/tv/caffeine/app/stage/ChatViewModelTests.kt
@@ -2,6 +2,7 @@ package tv.caffeine.app.stage
 
 import android.content.Context
 import android.content.res.Resources
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -9,21 +10,27 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import tv.caffeine.app.api.model.CaffeineEmptyResult
 import tv.caffeine.app.api.model.CaffeineResult
 import tv.caffeine.app.api.model.Message
+import tv.caffeine.app.api.model.MessageWrapper
 import tv.caffeine.app.api.model.SignedUserToken
 import tv.caffeine.app.api.model.User
 import tv.caffeine.app.auth.TokenStore
 import tv.caffeine.app.session.FollowManager
 
 class ChatViewModelTests {
+    @Rule
+    @JvmField val instantTaskExecutorRule = InstantTaskExecutorRule()
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
     lateinit var subject: ChatViewModel
@@ -72,5 +79,36 @@ class ChatViewModelTests {
         coEvery { endorseMessageUseCase(any()) } returns CaffeineEmptyResult.Success
         subject.endorseMessage(message)
         coVerify { endorseMessageUseCase("messageId") }
+    }
+
+    @Test
+    fun `rescind removes all messages by publisher`() {
+        every { user.caid } returns "CAID1"
+        val anotherUser = mockk<User>()
+        every { anotherUser.caid } returns "CAID2"
+        val message1 = MessageWrapper(Message(user, "1", Message.Type.reaction, Message.Body("body1"), 0), 1, 0)
+        val message2 = MessageWrapper(Message(anotherUser, "2", Message.Type.reaction, Message.Body("body2"), 0), 2, 0)
+        val message3 = MessageWrapper(Message(user, "3", Message.Type.reaction, Message.Body("body3"), 0), 3, 0)
+        val message4 = MessageWrapper(Message(user, "4", Message.Type.rescind, Message.Body("body4"), 0), 4, 0)
+        coEvery { messageController.connect(any()) } returns flowOf(message1, message2, message3, message4)
+        subject.messages.observeForever {
+            assertEquals(1, it.size)
+            assertEquals("CAID2", it.first().publisher.caid)
+        }
+    }
+
+    @Test
+    fun `reactions are all published`() {
+        every { user.caid } returns "CAID1"
+        val anotherUser = mockk<User>()
+        every { anotherUser.caid } returns "CAID2"
+        val message1 = MessageWrapper(Message(user, "1", Message.Type.reaction, Message.Body("body1"), 0), 1, 0)
+        val message2 = MessageWrapper(Message(anotherUser, "2", Message.Type.reaction, Message.Body("body2"), 0), 2, 0)
+        val message3 = MessageWrapper(Message(user, "3", Message.Type.reaction, Message.Body("body3"), 0), 3, 0)
+        val message4 = MessageWrapper(Message(anotherUser, "4", Message.Type.reaction, Message.Body("body4"), 0), 4, 0)
+        coEvery { messageController.connect(any()) } returns flowOf(message1, message2, message3, message4)
+        subject.messages.observeForever {
+            assertEquals(4, it.size)
+        }
     }
 }
