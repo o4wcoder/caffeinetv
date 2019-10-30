o4wcoder/caffeinetv

commit 8ebb9000265f6675418b6f2c32dbdb866513b541

[AND-443] [AND-427] [AND-412] Friends watching bottom sheet / fix ignored user text (#719)
* update release friends watching bottom sheet fragment, fix username color on ignored users screen

* PR changes, consistency, naming

* add `isDarkMode` nav arg to followers/following to handle text/star color

app/src/main/java/tv/caffeine/app/stage/FriendsWatchingFragment.kt
@@ -4,36 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import timber.log.Timber
-import tv.caffeine.app.MainNavDirections
 import tv.caffeine.app.R
-import tv.caffeine.app.api.model.User
+import tv.caffeine.app.api.model.CaidRecord
 import tv.caffeine.app.databinding.FragmentFriendsWatchingBinding
-import tv.caffeine.app.session.FollowManager
+import tv.caffeine.app.settings.ReleaseDesignConfig
 import tv.caffeine.app.ui.CaffeineBottomSheetDialogFragment
-import tv.caffeine.app.util.DispatchConfig
-import tv.caffeine.app.util.UsernameTheming
-import tv.caffeine.app.util.configure
-import tv.caffeine.app.util.safeNavigate
+import tv.caffeine.app.users.CaidListAdapter
+import tv.caffeine.app.util.FollowStarColor
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class FriendsWatchingFragment @Inject constructor(
-    private val usersAdapter: FriendsWatchingAdapter
+    private val userCaidAdapter: CaidListAdapter,
+    val releaseDesignConfig: ReleaseDesignConfig
 ) : CaffeineBottomSheetDialogFragment() {
 
     private val args by navArgs<FriendsWatchingFragmentArgs>()
@@ -43,76 +29,18 @@ class FriendsWatchingFragment @Inject constructor(
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentFriendsWatchingBinding.inflate(inflater, container, false)
-        binding.usersRecyclerView.adapter = usersAdapter
+        binding.isReleaseActive = releaseDesignConfig.isReleaseDesignActive()
+        userCaidAdapter.setNavController(activity?.findNavController(R.id.activity_main))
+        userCaidAdapter.setUsernameFollowStarColor(FollowStarColor.WHITE)
+        binding.usersRecyclerView.adapter = userCaidAdapter
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        usersAdapter.callback = { caid ->
-            val navController = activity?.findNavController(R.id.activity_main)
-            val action = MainNavDirections.actionGlobalProfileFragment(caid)
-            navController?.safeNavigate(action)
-            dismiss()
-        }
         val stageIdentifier = args.stageIdentifier
         friendsWatchingViewModel.load(stageIdentifier)
         friendsWatchingViewModel.friendsWatching.observe(viewLifecycleOwner, Observer {
-            usersAdapter.submitList(it)
+            userCaidAdapter.submitList(it.map { CaidRecord.FriendWatching(it.caid) })
         })
-    }
-}
-
-class FriendsWatchingAdapter @Inject constructor(
-    private val dispatchConfig: DispatchConfig,
-    private val followManager: FollowManager
-) : ListAdapter<User, FriendWatchingViewHolder>(
-        object : DiffUtil.ItemCallback<User?>() {
-            override fun areItemsTheSame(oldItem: User, newItem: User) = oldItem === newItem
-            override fun areContentsTheSame(oldItem: User, newItem: User) = oldItem.caid == newItem.caid
-        }
-), CoroutineScope {
-
-    var callback: ((String) -> Unit)? = null
-
-    private val job = SupervisorJob()
-    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        Timber.e(throwable, "Coroutine throwable")
-    }
-    override val coroutineContext: CoroutineContext
-        get() = dispatchConfig.main + job + exceptionHandler
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendWatchingViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item_search, parent, false)
-        return FriendWatchingViewHolder(view, followManager) { caid ->
-            callback?.invoke(caid)
-        }
-    }
-
-    override fun onBindViewHolder(holder: FriendWatchingViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
-    }
-
-    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
-        super.onDetachedFromRecyclerView(recyclerView)
-        job.cancel()
-    }
-}
-
-class FriendWatchingViewHolder(
-    itemView: View,
-    private val followManager: FollowManager,
-    private val callback: (String) -> Unit
-) : RecyclerView.ViewHolder(itemView) {
-
-    private val avatarImageView: ImageView = itemView.findViewById(R.id.avatar_image_view)
-    private val usernameTextView: TextView = itemView.findViewById(R.id.username_text_view)
-    private val followButton: Button = itemView.findViewById(R.id.follow_button)
-
-    fun bind(user: User) {
-        followButton.isVisible = false
-        user.configure(avatarImageView, usernameTextView, null, followManager, false, null, R.dimen.avatar_size,
-            UsernameTheming.STANDARD)
-        itemView.setOnClickListener { callback(user.caid) }
     }
 }

app/src/main/java/tv/caffeine/app/users/CaidListAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -29,6 +30,8 @@ import tv.caffeine.app.settings.ReleaseDesignConfig
 import tv.caffeine.app.ui.FollowListAdapter
 import tv.caffeine.app.ui.FollowStarViewModel
 import tv.caffeine.app.ui.LiveStatusIndicatorViewModel
+import tv.caffeine.app.ui.configureUserIcon
+import tv.caffeine.app.ui.loadAvatar
 import tv.caffeine.app.util.DispatchConfig
 import tv.caffeine.app.util.FollowStarColor
 import tv.caffeine.app.util.UsernameTheming
@@ -55,12 +58,15 @@ class CaidListAdapter @Inject constructor(
     override val coroutineContext: CoroutineContext
         get() = dispatchConfig.main + job + exceptionHandler
 
+    private var navController: NavController? = null
+    private var usernameFollowStarColor: FollowStarColor = FollowStarColor.WHITE
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         if (releaseDesignConfig.isReleaseDesignActive()) {
             val context = parent.context
             val inflater = LayoutInflater.from(context)
             val binding = DataBindingUtil.inflate<CaidItemBinding>(inflater, R.layout.caid_item, parent, false)
-            ReleaseCaidViewHolder(binding, FollowManager.FollowHandler(fragmentManager, callback), this, ::onFollowStarClick)
+            ReleaseCaidViewHolder(binding, FollowManager.FollowHandler(fragmentManager, callback), this, usernameFollowStarColor, ::onFollowStarClick)
         } else {
             val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item_search, parent, false)
             ClassicCaidViewHolder(view, FollowManager.FollowHandler(fragmentManager, callback), this)
@@ -71,13 +77,21 @@ class CaidListAdapter @Inject constructor(
         if (holder is ClassicCaidViewHolder) {
             holder.bind(item, followManager)
         } else {
-            (holder as ReleaseCaidViewHolder).bind(item, followManager, profileRepository)
+            (holder as ReleaseCaidViewHolder).bind(item, followManager, profileRepository, navController)
         }
     }
 
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
         super.onDetachedFromRecyclerView(recyclerView)
         job.cancel()
+    }
+
+    fun setNavController(navController: NavController?) {
+        this.navController = navController
+    }
+
+    fun setUsernameFollowStarColor(color: FollowStarColor) {
+        this.usernameFollowStarColor = color
     }
 }
 
@@ -121,6 +135,7 @@ class ReleaseCaidViewHolder(
     private val binding: CaidItemBinding,
     private val followHandler: FollowManager.FollowHandler,
     private val scope: CoroutineScope,
+    private val usernameFollowStarColor: FollowStarColor,
     onFollowStarClick: (user: User, isFollowing: Boolean) -> Unit
 ) :
     RecyclerView.ViewHolder(binding.root) {
@@ -128,20 +143,32 @@ class ReleaseCaidViewHolder(
     var followButton: Button? = null
 
     init {
-        binding.followStarViewModel = FollowStarViewModel(itemView.context, FollowStarColor.WHITE, onFollowStarClick)
+        binding.followStarViewModel = FollowStarViewModel(itemView.context, usernameFollowStarColor, onFollowStarClick)
         binding.liveStatusIndicatorViewModel = LiveStatusIndicatorViewModel()
     }
 
     var job: Job? = null
 
-    fun bind(item: CaidRecord, followManager: FollowManager, profileRepository: ProfileRepository) {
+    /*
+    If this adapter is being used in a BottomSheetDialogFragment, then it wont have a navController and the navController from the dialog's
+    activity will have to be passed to this bind function.
+     */
+    fun bind(item: CaidRecord, followManager: FollowManager, profileRepository: ProfileRepository, navController: NavController? = null) {
         job?.cancel()
         clear()
         job = scope.launch {
             val user = followManager.userDetails(item.caid) ?: return@launch
             val userProfile = profileRepository.getUserProfile(user.username)
-            user.configure(binding.avatarImageView, binding.usernameTextView, followButton, followManager, true, followHandler, R.dimen.avatar_size,
-                UsernameTheming.STANDARD_DARK)
+            binding.avatarImageView.loadAvatar(user.avatarImageUrl, false, R.dimen.avatar_size, true)
+            binding.usernameTextView.apply {
+                text = user.username
+                setTextColor(resources.getColor(usernameFollowStarColor.color, null))
+                configureUserIcon(when {
+                    user.isVerified -> R.drawable.verified
+                    user.isCaster -> R.drawable.caster
+                    else -> 0
+                })
+            }
 
             userProfile?.let { binding.liveStatusIndicatorViewModel?.isUserLive = it.isLive }
 
@@ -154,7 +181,7 @@ class ReleaseCaidViewHolder(
         }
         itemView.setOnClickListener {
             val action = MainNavDirections.actionGlobalProfileFragment(item.caid)
-            itemView.findNavController().safeNavigate(action)
+            navController?.let { it.safeNavigate(action) } ?: itemView.findNavController().safeNavigate(action)
         }
     }
 

app/src/main/java/tv/caffeine/app/profile/MyProfileFragment.kt
@@ -185,13 +185,13 @@ class MyProfileFragment @Inject constructor(
 
     private fun showFollowingList() {
         val caid = tokenStore.caid ?: return
-        val action = MyProfileFragmentDirections.actionMyProfileFragmentToFollowingFragment(caid)
+        val action = MyProfileFragmentDirections.actionMyProfileFragmentToFollowingFragment(caid, false)
         findNavController().safeNavigate(action)
     }
 
     private fun showFollowersList() {
         val caid = tokenStore.caid ?: return
-        val action = MyProfileFragmentDirections.actionMyProfileFragmentToFollowersFragment(caid)
+        val action = MyProfileFragmentDirections.actionMyProfileFragmentToFollowersFragment(caid, false)
         findNavController().safeNavigate(action)
     }
 

app/src/main/java/tv/caffeine/app/profile/ProfileFragment.kt
@@ -86,12 +86,12 @@ class ProfileFragment : CaffeineFragment(R.layout.fragment_profile) {
     }
 
     private fun showFollowingList() {
-        val action = ProfileFragmentDirections.actionProfileFragmentToFollowingFragment(caid)
+        val action = ProfileFragmentDirections.actionProfileFragmentToFollowingFragment(caid, false)
         findNavController().safeNavigate(action)
     }
 
     private fun showFollowersList() {
-        val action = ProfileFragmentDirections.actionProfileFragmentToFollowersFragment(caid)
+        val action = ProfileFragmentDirections.actionProfileFragmentToFollowersFragment(caid, false)
         findNavController().safeNavigate(action)
     }
 

app/src/main/java/tv/caffeine/app/stage/StageBroadcastProfilePagerFragment.kt
@@ -84,8 +84,8 @@ class StageBroadcastProfilePagerAdapter @AssistedInject constructor(
     override fun getItem(position: Int): Fragment =
         when (position) {
             0 -> { biographyFragmentProvider.get().apply { arguments = BiographyFragmentArgs(caid).toBundle() } }
-            1 -> { followersFragmentProvider.get().apply { arguments = FollowersFragmentArgs(caid).toBundle() } }
-            2 -> { followingFragmentProvider.get().apply { arguments = FollowingFragmentArgs(caid).toBundle() } }
+            1 -> { followersFragmentProvider.get().apply { arguments = FollowersFragmentArgs(caid, true).toBundle() } }
+            2 -> { followingFragmentProvider.get().apply { arguments = FollowingFragmentArgs(caid, true).toBundle() } }
 
             else -> throw IllegalStateException("Unknown exception")
         }

app/src/main/java/tv/caffeine/app/ui/PicassoExt.kt
@@ -61,13 +61,15 @@ fun RequestCreator.maybeRoundedCorners(cornerRadius: Float?): RequestCreator {
     return transform(RoundedCornersTransformation(cornerRadius.toInt(), 0))
 }
 
-fun ImageView.loadAvatar(avatarImageUrl: String, isFollowing: Boolean, @DimenRes avatarImageSizeRes: Int) {
+fun ImageView.loadAvatar(avatarImageUrl: String, isFollowing: Boolean, @DimenRes avatarImageSizeRes: Int, hasRim: Boolean = false) {
     val resources = context.resources
     val isAvatarLarge = resources.getDimension(avatarImageSizeRes) >
             resources.getDimension(R.dimen.avatar_rim_size_threshold_on_avatar_size)
     val rimSize = resources.getDimensionPixelSize(
             if (isFollowing) {
                 if (isAvatarLarge) R.dimen.avatar_rim_size_large else R.dimen.avatar_rim_size_regular
+            } else if (hasRim) {
+                R.dimen.avatar_no_rim
             } else {
                 R.dimen.avatar_rim_size_not_following
             })

app/src/main/java/tv/caffeine/app/users/FollowersFragment.kt
@@ -22,6 +22,7 @@ import tv.caffeine.app.api.model.CaidRecord
 import tv.caffeine.app.api.model.awaitAndParseErrors
 import tv.caffeine.app.databinding.UserListFragmentBinding
 import tv.caffeine.app.ui.CaffeineFragment
+import tv.caffeine.app.util.FollowStarColor
 import tv.caffeine.app.util.setItemDecoration
 import javax.inject.Inject
 
@@ -36,6 +37,8 @@ class FollowersFragment @Inject constructor(
         val binding = UserListFragmentBinding.bind(view)
         binding.lifecycleOwner = viewLifecycleOwner
         caidListAdapter.fragmentManager = fragmentManager
+        val usernameFollowstarColor = if (args.isDarkMode) FollowStarColor.WHITE else FollowStarColor.BLACK
+        caidListAdapter.setUsernameFollowStarColor(usernameFollowstarColor)
         binding.userListRecyclerView.apply {
             adapter = caidListAdapter
             setItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))

commit 943949be529dad59bfb6b1198efc36355fbb2432

[AND-582] Fix the crashes on the stage friends watching list (#872)
- The first crash is when the user taps on any list item in
  FriendsWatchingFragment in the classic UI.
- The second crash is when the user rotates the screen in
  FriendsWatchingFragment in either the classic or release UI.
- Crashes introduced in 8ebb9000.
- Use a callback rather than passing the navController to the adapter
  because the navController changes after the screen rotation.
- Tested FriendsWatchingFragment manually on both the classic and release UI.
- Tested the follower/following/ignored list.

app/src/main/java/tv/caffeine/app/stage/FriendsWatchingFragment.kt
@@ -6,7 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import tv.caffeine.app.R
 import tv.caffeine.app.api.model.CaidRecord
@@ -14,7 +15,9 @@ import tv.caffeine.app.databinding.FragmentFriendsWatchingBinding
 import tv.caffeine.app.settings.ReleaseDesignConfig
 import tv.caffeine.app.ui.CaffeineBottomSheetDialogFragment
 import tv.caffeine.app.users.CaidListAdapter
+import tv.caffeine.app.users.UserNavigationCallback
 import tv.caffeine.app.util.ThemeColor
+import tv.caffeine.app.util.safeNavigate
 import javax.inject.Inject
 
 class FriendsWatchingFragment @Inject constructor(
@@ -30,7 +33,12 @@ class FriendsWatchingFragment @Inject constructor(
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentFriendsWatchingBinding.inflate(inflater, container, false)
         binding.isReleaseActive = releaseDesignConfig.isReleaseDesignActive()
-        userCaidAdapter.setNavController(activity?.findNavController(R.id.activity_main))
+        val userNavigationCallback = object : UserNavigationCallback {
+            override fun onUserNavigation(action: NavDirections) {
+                findNavController().safeNavigate(action)
+            }
+        }
+        userCaidAdapter.setUserNavigationCallback(userNavigationCallback)
         userCaidAdapter.setUsernameFollowStarColor(ThemeColor.DARK)
         binding.usersRecyclerView.adapter = userCaidAdapter
         return binding.root

app/src/main/java/tv/caffeine/app/users/CaidListAdapter.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -58,7 +58,7 @@ class CaidListAdapter @Inject constructor(
     override val coroutineContext: CoroutineContext
         get() = dispatchConfig.main + job + exceptionHandler
 
-    private var navController: NavController? = null
+    private var userNavigationCallback: UserNavigationCallback? = null
     private var usernameThemeColor: ThemeColor = ThemeColor.DARK
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
@@ -74,10 +74,9 @@ class CaidListAdapter @Inject constructor(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = getItem(position)
-        if (holder is ClassicCaidViewHolder) {
-            holder.bind(item, followManager)
-        } else {
-            (holder as ReleaseCaidViewHolder).bind(item, followManager, profileRepository, navController)
+        when (holder) {
+            is ClassicCaidViewHolder -> holder.bind(item, followManager, userNavigationCallback)
+            is ReleaseCaidViewHolder -> holder.bind(item, followManager, profileRepository, userNavigationCallback)
         }
     }
 
@@ -86,8 +85,8 @@ class CaidListAdapter @Inject constructor(
         job.cancel()
     }
 
-    fun setNavController(navController: NavController?) {
-        this.navController = navController
+    fun setUserNavigationCallback(callback: UserNavigationCallback) {
+        userNavigationCallback = callback
     }
 
     fun setUsernameFollowStarColor(color: ThemeColor) {
@@ -96,14 +95,17 @@ class CaidListAdapter @Inject constructor(
 }
 
 class ClassicCaidViewHolder(itemView: View, private val followHandler: FollowManager.FollowHandler, private val scope: CoroutineScope) :
-    RecyclerView.ViewHolder(itemView) {
-    private val avatarImageView: ImageView = itemView.findViewById(R.id.avatar_image_view)
-    private val usernameTextView: TextView = itemView.findViewById(R.id.username_text_view)
-    private val followButton: Button = itemView.findViewById(R.id.follow_button)
+    RecyclerView.ViewHolder(itemView), UserNavigable {
+    private lateinit var avatarImageView: ImageView
+    private lateinit var usernameTextView: TextView
+    private lateinit var followButton: Button
 
     var job: Job? = null
 
-    fun bind(item: CaidRecord, followManager: FollowManager) {
+    fun bind(item: CaidRecord, followManager: FollowManager, userNavigationCallback: UserNavigationCallback?) {
+        avatarImageView = itemView.findViewById(R.id.avatar_image_view)
+        usernameTextView = itemView.findViewById(R.id.username_text_view)
+        followButton = itemView.findViewById(R.id.follow_button)
         job?.cancel()
         clear()
         job = scope.launch {
@@ -113,9 +115,9 @@ class ClassicCaidViewHolder(itemView: View, private val followHandler: FollowMan
             user.configure(avatarImageView, usernameTextView, maybeFollowButton, followManager, true, followHandler, R.dimen.avatar_size,
                     UsernameTheming.STANDARD)
         }
-        itemView.setOnClickListener {
+        itemView.setOnClickListener { view ->
             val action = MainNavDirections.actionGlobalProfileFragment(item.caid)
-            itemView.findNavController().safeNavigate(action)
+            performUserNavigation(action, userNavigationCallback, view)
         }
     }
 
@@ -137,7 +139,7 @@ class ReleaseCaidViewHolder(
     private val usernameThemeColor: ThemeColor,
     onFollowStarClick: (caid: CAID, isFollowing: Boolean) -> Unit
 ) :
-    RecyclerView.ViewHolder(binding.root) {
+    RecyclerView.ViewHolder(binding.root), UserNavigable {
     @VisibleForTesting
     var followButton: Button? = null
 
@@ -152,7 +154,12 @@ class ReleaseCaidViewHolder(
     If this adapter is being used in a BottomSheetDialogFragment, then it wont have a navController and the navController from the dialog's
     activity will have to be passed to this bind function.
      */
-    fun bind(item: CaidRecord, followManager: FollowManager, profileRepository: ProfileRepository, navController: NavController? = null) {
+    fun bind(
+        item: CaidRecord,
+        followManager: FollowManager,
+        profileRepository: ProfileRepository,
+        userNavigationCallback: UserNavigationCallback?
+    ) {
         job?.cancel()
         clear()
         job = scope.launch {
@@ -184,11 +191,10 @@ class ReleaseCaidViewHolder(
                 binding.followStarViewModel!!.bind(user.caid, isFollowing, isSelf)
                 binding.executePendingBindings()
             }
-            itemView.setOnClickListener {
-                userProfile?.username?.let {
-                    val action = MainNavDirections.actionGlobalStagePagerFragment(userProfile.username)
-                    navController?.let { it.safeNavigate(action) }
-                        ?: itemView.findNavController().safeNavigate(action)
+            itemView.setOnClickListener { view ->
+                userProfile?.username?.let { username ->
+                    val action = MainNavDirections.actionGlobalStagePagerFragment(username)
+                    performUserNavigation(action, userNavigationCallback, view)
                 }
             }
         }
@@ -200,5 +206,28 @@ class ReleaseCaidViewHolder(
         binding.followStarViewModel?.hide()
         binding.liveStatusIndicatorViewModel?.isUserLive = false
         binding.root.setOnClickListener(null)
+    }
+}
+
+/**
+ * Use the callback on screens where the nav controller is not supported.
+ * E.g., [FriendsWatchingFragment] is a DialogFragment. Only [NavHostFragment] is supported.
+ */
+interface UserNavigationCallback {
+    fun onUserNavigation(action: NavDirections) {
+    }
+}
+
+/**
+ * An interface to encapsulate the logic to a user stage or profile.
+ * @see [UserNavigationCallback].
+ */
+interface UserNavigable {
+    fun performUserNavigation(
+        action: NavDirections,
+        userNavigationCallback: UserNavigationCallback?,
+        view: View
+    ) {
+        userNavigationCallback?.onUserNavigation(action) ?: view.findNavController().safeNavigate(action)
     }
 }

app/src/test/java/tv/caffeine/app/users/CaidViewHolderTests.kt
@@ -1,0 +1,85 @@
+package tv.caffeine.app.users
+
+import android.content.Context
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockkStatic
+import io.mockk.verify
+import kotlinx.coroutines.CoroutineScope
+import org.junit.Before
+import org.junit.Test
+import tv.caffeine.app.MainNavDirections
+import tv.caffeine.app.databinding.CaidItemBinding
+import tv.caffeine.app.session.FollowManager
+import tv.caffeine.app.util.ThemeColor
+import tv.caffeine.app.util.safeNavigate
+
+class CaidViewHolderTests {
+
+    private lateinit var classicCaidViewHolder: ClassicCaidViewHolder
+    private lateinit var releaseCaidViewHolder: ReleaseCaidViewHolder
+    @MockK private lateinit var itemView: View
+    @MockK private lateinit var followHandler: FollowManager.FollowHandler
+    @MockK private lateinit var scope: CoroutineScope
+    @MockK private lateinit var binding: CaidItemBinding
+    @MockK private lateinit var context: Context
+    @MockK private lateinit var navController: NavController
+    @MockK private lateinit var userNavigationCallback: UserNavigationCallback
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        mockkStatic(Navigation::class)
+        every { Navigation.findNavController(any()) } returns navController
+        every { binding.root } returns itemView
+        every { binding.followStarViewModel = any() } just Runs
+        every { binding.liveStatusIndicatorViewModel = any() } just Runs
+        every { itemView.context } returns context
+        every { userNavigationCallback.onUserNavigation(any()) } just Runs
+
+        classicCaidViewHolder = ClassicCaidViewHolder(itemView, followHandler, scope)
+        releaseCaidViewHolder = ReleaseCaidViewHolder(binding, scope, ThemeColor.DARK) { _, _ -> }
+    }
+
+    @Test
+    fun `the user navigation callback is called instead of the nav controller from the view if the callback is not null in the classic view holder`() {
+        val action = MainNavDirections.actionGlobalProfileFragment("caid")
+        every { navController.navigate(action) } just Runs
+        classicCaidViewHolder.performUserNavigation(action, userNavigationCallback, itemView)
+        verify(exactly = 1) { userNavigationCallback.onUserNavigation(action) }
+        verify(exactly = 0) { navController.safeNavigate(action) }
+    }
+
+    @Test
+    fun `the user navigation callback is called instead of the nav controller from the view if the callback is not null in the release view holder`() {
+        val action = MainNavDirections.actionGlobalStagePagerFragment("username")
+        every { navController.navigate(action) } just Runs
+        releaseCaidViewHolder.performUserNavigation(action, userNavigationCallback, itemView)
+        verify(exactly = 1) { userNavigationCallback.onUserNavigation(action) }
+        verify(exactly = 0) { navController.safeNavigate(action) }
+    }
+
+    @Test
+    fun `the nav controller from the view will be called instead of the callback if the callback is null in the classic view holder`() {
+        val action = MainNavDirections.actionGlobalProfileFragment("caid")
+        every { navController.navigate(action) } just Runs
+        classicCaidViewHolder.performUserNavigation(action, null, itemView)
+        verify(exactly = 0) { userNavigationCallback.onUserNavigation(action) }
+        verify(exactly = 1) { navController.safeNavigate(action) }
+    }
+
+    @Test
+    fun `the nav controller from the view will be called instead of the callback if the callback is null in the release view holder`() {
+        val action = MainNavDirections.actionGlobalStagePagerFragment("username")
+        every { navController.navigate(action) } just Runs
+        releaseCaidViewHolder.performUserNavigation(action, null, itemView)
+        verify(exactly = 0) { userNavigationCallback.onUserNavigation(action) }
+        verify(exactly = 1) { navController.safeNavigate(action) }
+    }
+}